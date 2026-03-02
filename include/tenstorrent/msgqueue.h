3599b3b1761af2e62eeaede380717c63304fa01d
Andrew Lewycky <alewycky@tenstorrent.com>
2026-03-02 12:32:35 -0500

    MSG_TYPE_MODIFY_THROTTLER

--- include/tenstorrent/msgqueue.h
+++ include/tenstorrent/msgqueue.h
@@ -343,6 +343,26 @@ struct asic_state_rqst {
 	uint8_t command_code;
 };
 
+/** @brief Host request to modify throttler parameters
+ * @details Requests of this type are processed by @ref ModifyThrottlerHandler
+ */
+struct modify_throttler_rqst {
+	/** @brief The command code corresponding TT_SMC_MSG_MODIFY_THROTTLER */
+	uint8_t command_code;
+	/** @brief Throttler index, from enum ThrottlerId */
+	uint8_t throttler_id;
+	uint8_t pad[2];
+
+	/** @brief Proportional gain */
+	float p_gain;
+	/** @brief Derivative gain */
+	float d_gain;
+	/** @brief Alpha factor */
+	float alpha_factor;
+	/** @brief Throttler trigger point */
+	float limit;
+};
+
 /** @brief A tenstorrent host request*/
 union request {
 	/** @brief The interpretation of the request as an array of uint32_t entries*/
@@ -400,6 +420,9 @@ union request {
 
 	/** @brief An ASIC state transition request */
 	struct asic_state_rqst asic_state;
+
+	/** @brief A throttler parameter modification request */
+	struct modify_throttler_rqst modify_throttler;
 };
 
 /** @} */
